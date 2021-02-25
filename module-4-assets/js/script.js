a3289b6be908d399f3c4428869305629afed689b
Sergio Torres <sergio.lo.torres@gmail.com>
2021-02-25 01:43:44 -0600

    added functionality to form

--- module-4-assets/js/script.js
+++ module-4-assets/js/script.js
@@ -1,14 +1,32 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#task-to-do");
 
 //we have now listed both of the elements in our DOM we are working on: The button and the task cell.
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name ='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //create list item 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent= "This is a new task";
+    
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    //give it a class name
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>"+ taskNameInput + "</h3><span class='task-type'>"+ taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+    
+    //add entire list item to list
     taskToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click",createTaskHandler)
+formEl.addEventListener("submit",createTaskHandler)
 
